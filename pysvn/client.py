d69a0f255e3a78a43c28a847cb603939de793e82
Zohar Lee <leafvmaple@gmail.com>
2021-01-19 21:38:41 +0800

    Add log & diff.

--- pysvn/client.py
+++ pysvn/client.py
@@ -2,13 +2,21 @@ import subprocess
 import os
 
 class Client:
-    def __init__(self):
+    def __init__(self, cwd = os.getcwd(), stdout = subprocess.PIPE):
         self.cmd = "svn"
-        self.log = None
-        self.cwd = os.getcwd()
-        self.stdout = subprocess.PIPE
-        
-    def log(self, decode='utf8'):
-        if self.log is None:
-            self.log = subprocess.Popen([self.cmd, "log"], stdout = self.stdout, cwd = self.cwd).stdout.read()
-        return bytes.decode(v, decode)
+        self.log_content = None
+        self.cwd = cwd
+        self.stdout = stdout
+
+    def log(self, decoding='utf8'):
+        if self.log_content is None:
+            self.log_content = subprocess.Popen([self.cmd, "log"], stdout = self.stdout, cwd = self.cwd).stdout.read()
+        return bytes.decode(self.log_content, decoding)
+
+    def diff(self, start_version, end_version = None, decoding='utf8'):
+        if end_version is None:
+            end_version = start_version
+            start_version = str(int(v[1:]) - 1)
+        diff_cmd = "{0} diff -r {1}:{2}".format(start_version, end_version)
+        diff_content = subprocess.Popen([self.cmd, diff_cmd], stdout = self.stdout, cwd = self.cwd).stdout.read()
+        return bytes.decode(diff_content, decoding)
